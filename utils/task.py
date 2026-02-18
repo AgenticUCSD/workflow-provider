91f8f62c021c8b5547d921f0259085b1e8f2e8d5
eaniket <singhaniket406@gmail.com>
2026-02-18 09:54:32 -0800

    Updating task definition

--- utils/task.py
+++ utils/task.py
@@ -1,6 +1,6 @@
 from enum import Enum
 from pydantic import BaseModel
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, Optional
 
 class TaskTypes(str, Enum):
     NO_TASK = "notask"
@@ -21,21 +21,29 @@ class Objective(BaseModel):
     name: str
     description: str
     inputs: Dict[str, Any]
-    constraints: Optional[Dict[str, Any]] = None
+    constrainsts: Optional[Dict[str, Any]] = None
     success_criteria: str
     expected_output: Dict[str, Any]
     deadline: Optional[str] = None  # ISO 8601 format
+
+#TODO: The definition of step and workflow needs refinement. Currently it's a sample structure
+class Step(BaseModel):
+    claude_skill_name: str
+    api: str
+    access: str #can be changed to str | AccessLevel
 
 class Workflow(BaseModel):
     workflow_id: str
     name: str
     description: str
-    steps: List[str]
+    steps: list[Step]  # Define the structure of steps as needed
+
     
 class Task(BaseModel):
     task_id: str
     task_type: TaskTypes
-    objective: Objective
-    workflow: Optional[Workflow] = None
+    objective: Objective 
+    candidate_workflow: list[Workflow] #possible workflows
+    workflow: Workflow
     status: Status
     metadata: Optional[Dict[str, Any]] = None
